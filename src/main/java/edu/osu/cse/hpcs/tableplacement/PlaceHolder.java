25ac6f780af3b515c82a1cfc387c45219d76db68
yhuai <huaiyin.thu@gmail.com>
2012-11-06 16:30:01 -0500

    initial commit

---
+++ src/main/java/edu/osu/cse/hpcs/tableplacement/PlaceHolder.java
@@ -0,0 +1,13 @@
+package edu.osu.cse.hpcs.tableplacement;
+
+public class PlaceHolder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/edu/osu/cse/hpcs/tableplacement/TestPlaceHolder.java
@@ -0,0 +1,13 @@
+package edu.osu.cse.hpcs.tableplacement;
+
+public class TestPlaceHolder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
