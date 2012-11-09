912adee4d791d0da9281f4254b6383635286aca8
yhuai <huaiyin.thu@gmail.com>
2012-11-09 12:10:33 -0500

    work in progress

--- tableplacement-experiment/src/main/java/edu/osu/cse/hpcs/tableplacement/TableProperty.java
+++ tableplacement-experiment/src/main/java/edu/osu/cse/hpcs/tableplacement/TableProperty.java
@@ -3,24 +3,46 @@ package edu.osu.cse.hpcs.tableplacement;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.hadoop.hive.ql.io.RCFile;
+import org.apache.hadoop.hive.serde.Constants;
+import org.apache.hadoop.hive.serde2.typeinfo.TypeInfo;
+import org.apache.hadoop.hive.serde2.typeinfo.TypeInfoUtils;
 import org.apache.log4j.Logger;
+
+import edu.osu.cse.hpcs.tableplacement.column.Column;
+import edu.osu.cse.hpcs.tableplacement.column.DataType;
+import edu.osu.cse.hpcs.tableplacement.column.DoubleColumn;
+import edu.osu.cse.hpcs.tableplacement.column.IntColumn;
+import edu.osu.cse.hpcs.tableplacement.column.MapColumn;
+import edu.osu.cse.hpcs.tableplacement.column.StringColumn;
+import edu.osu.cse.hpcs.tableplacement.exception.TablePropertyException;
 
 public class TableProperty {
 
   // General properties
-  public final static String COLUMN_NAME_STR = "column.name.string";
-  public final static String COLUMN_TYPE_STR = "column.type.string";
   public final static String HADOOP_IO_BUFFER_SIZE = "io.file.buffer.size";
+
+  // Default values of general properties
+  public final static int DEFAULT_HADOOP_IO_BUFFER_SIZE = 131072; // 128KB
+
+  // Hive properties
+  // Refer to org.apache.hadoop.hive.serde.Constants
 
   // RCFile properties
   public final static String RCFILE_ROWGROUP_SIZE_STR = RCFile.Writer.COLUMNS_BUFFER_SIZE_CONF_STR;
 
+  // Default values of RCFile properties
+  public final static int DEFAULT_RCFILE_ROWGROUP_SIZE_STR = 16777216; // 16MB
+
   Logger log = Logger.getLogger(TableProperty.class);
 
   private Properties prop;
+  private File propsFile;
 
   private TableProperty() {
     prop = new Properties();
@@ -30,13 +52,122 @@ public class TableProperty {
    * Load a table property from a file.
    * 
    * @throws IOException
+   * @throws TablePropertyException
    */
-  public TableProperty(File propsFile) throws IOException {
+  public TableProperty(File propsFile) throws IOException,
+      TablePropertyException {
     this();
+    this.propsFile = propsFile;
     log.info("Load table property file from " + propsFile.getPath());
     FileInputStream fis = new FileInputStream(propsFile);
     prop.load(fis);
     fis.close();
+  }
+
+  public void set(String key, String value) {
+    prop.setProperty(key, value);
+  }
+
+  public String get(String key) {
+    return prop.getProperty(key);
+  }
+
+  public void setInt(String key, int value) {
+    prop.setProperty(key, Integer.toString(value));
+  }
+
+  public int getInt(String key, int defaultValue) {
+    String valueString = prop.getProperty(key);
+    if (valueString == null)
+      return defaultValue;
+    try {
+      return Integer.parseInt(valueString);
+    } catch (NumberFormatException e) {
+      return defaultValue;
+    }
+  }
+
+  public void setLong(String key, long value) {
+    prop.setProperty(key, Long.toString(value));
+  }
+
+  public long getLong(String key, long defaultValue) {
+    String valueString = prop.getProperty(key);
+    if (valueString == null)
+      return defaultValue;
+    try {
+      return Long.parseLong(valueString);
+    } catch (NumberFormatException e) {
+      return defaultValue;
+    }
+  }
+
+  public void setBoolean(String key, boolean value) {
+    prop.setProperty(key, Boolean.toString(value));
+  }
+
+  public boolean getBoolean(String key, boolean defaultValue) {
+    String valueString = prop.getProperty(key);
+    if (valueString == null)
+      return defaultValue;
+    return Boolean.parseBoolean(valueString);
+  }
+
+  public List<Column> getColumns() throws TablePropertyException {
+    String columnsStr = get(Constants.LIST_COLUMNS);
+    if (columnsStr == null || columnsStr.length() == 0) {
+      throw new TablePropertyException("Columns (key: "
+          + Constants.LIST_COLUMNS + ") are not defined in the property file "
+          + propsFile.getPath());
+    }
+    List<String> columnNames = Arrays.asList(columnsStr.split(","));
+    log.info("columnNames is " + columnNames);
+
+    String columnsTypesStr = get(Constants.LIST_COLUMN_TYPES);
+    if (columnsTypesStr == null || columnsTypesStr.length() == 0) {
+      throw new TablePropertyException("Column types (key: "
+          + Constants.LIST_COLUMN_TYPES
+          + ") are not defined in the property file " + propsFile.getPath());
+    }
+    List<TypeInfo> columnTypes = TypeInfoUtils
+        .getTypeInfosFromTypeString(columnsTypesStr);
+    log.info("columnTypes is " + columnTypes);
+
+    if (columnNames.size() != columnTypes.size()) {
+      throw new TablePropertyException(
+          "The number of column names and that of column types are not equal");
+    }
+
+    List<Column> columns = new ArrayList<Column>();
+
+    for (int i = 0; i < columnNames.size(); i++) {
+      String name = columnNames.get(i);
+      TypeInfo type = columnTypes.get(i);
+
+      if (DataType.INT_STR.equals(type.getTypeName())) {
+        Column column = new IntColumn(name, this);
+        columns.add(column);
+      } else if (DataType.DOUBLE_STR.equals(type.getTypeName())) {
+        Column column = new DoubleColumn(name, this);
+        columns.add(column);
+      } else if (DataType.STRING_STR.equals(type.getTypeName())) {
+        Column column = new StringColumn(name, this);
+        columns.add(column);
+      } else if (type.getTypeName().startsWith(DataType.MAP_STR)) {
+        String typeStr = type.getTypeName();
+        String[] kv = typeStr.substring(4, typeStr.length()-1).split(",");
+        assert kv.length == 2;
+        String keyType = kv[0];
+        String valueType = kv[1];
+        Column column = new MapColumn(name, keyType, valueType, this);
+        columns.add(column);
+      } else {
+        throw new TablePropertyException("The type " + type.getTypeName()
+            + " provided in " + this.propsFile.getPath() + " is not supported");
+      }
+    }
+
+    return columns;
   }
 
 }
--- tableplacement-experiment/src/test/java/edu/osu/cse/hpcs/tableplacement/TestTableProperty.java
+++ tableplacement-experiment/src/test/java/edu/osu/cse/hpcs/tableplacement/TestTableProperty.java
@@ -1,11 +1,66 @@
 package edu.osu.cse.hpcs.tableplacement;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.hive.serde.Constants;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Test;
 
-public class TestTableProperty {
+import edu.osu.cse.hpcs.tableplacement.column.Column;
+import edu.osu.cse.hpcs.tableplacement.exception.TablePropertyException;
+
+public class TestTableProperty extends TestBase {
+
+  Logger log = Logger.getLogger(TestTableProperty.class);
+  
+  TableProperty testTableProperty;
+  
+  public TestTableProperty() throws URISyntaxException, IOException, TablePropertyException {
+    super();
+    ClassLoader loader = Thread.currentThread().getContextClassLoader();
+    URL url = loader.getResource("testTableProperty.properties");
+    File file = new File(url.toURI());
+    testTableProperty = new TableProperty(file);
+  }
 
   @Test
-  public void testLoadTableProperty () {
+  public void testLoadTableProperty() throws TablePropertyException{
+    final String hiveColumnNames = "cint,cdouble,cstring,cmap1,cmap2,cmap3";
+    final String hvieColumnTypes = "int:double:string:map<int,string>:map<string,string>:map<string,double>";
+    final int ioBufferSize = 524288;
+    final int rcfileRowGroupSize = 4194304;
     
+    List<String> expectedColumnStr = new ArrayList<String>();
+    expectedColumnStr.add("Column[name:cint, type:INT, random: IntRandom[range=2147483647]]");
+    expectedColumnStr.add("Column[name:cdouble, type:DOUBLE, random: DoubleRandom[range=100000]]");
+    expectedColumnStr.add("Column[name:cstring, type:STRING, random: StringRandom[length=30]]");
+    expectedColumnStr.add("Column[name:cmap1, type:MAP, keyRandom: IntRandom[range=65535], valueRandom: StringRandom[length=4], size: 10]");
+    expectedColumnStr.add("Column[name:cmap2, type:MAP, keyRandom: StringRandom[length=4], valueRandom: StringRandom[length=4], size: 10]");
+    expectedColumnStr.add("Column[name:cmap3, type:MAP, keyRandom: StringRandom[length=4], valueRandom: DoubleRandom[range=100000], size: 10]");
+
+    Assert.assertEquals(hiveColumnNames,
+        testTableProperty.get(Constants.LIST_COLUMNS));
+    Assert.assertEquals(hvieColumnTypes,
+        testTableProperty.get(Constants.LIST_COLUMN_TYPES));
+    Assert.assertEquals(ioBufferSize, testTableProperty.getInt(
+        TableProperty.HADOOP_IO_BUFFER_SIZE,
+        TableProperty.DEFAULT_HADOOP_IO_BUFFER_SIZE));
+    Assert.assertEquals(rcfileRowGroupSize, testTableProperty.getInt(
+        TableProperty.RCFILE_ROWGROUP_SIZE_STR,
+        TableProperty.DEFAULT_RCFILE_ROWGROUP_SIZE_STR));
+    
+    List<Column> ret = testTableProperty.getColumns();
+    
+    Assert.assertEquals(expectedColumnStr.size(), ret.size());
+    
+    for (int i=0; i<ret.size(); i++) {
+      Assert.assertEquals(expectedColumnStr.get(i), ret.get(i).toString());
+    }
   }
 }
