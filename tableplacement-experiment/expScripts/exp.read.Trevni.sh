13a2b744071cecda64a3101ffc85e517585b1d18
yhuai <huai@cse.ohio-state.edu>
2013-01-24 22:52:40 -0500

    wip

--- tableplacement-experiment/expScripts/exp.read.Trevni.sh
+++ tableplacement-experiment/expScripts/exp.read.Trevni.sh
@@ -27,9 +27,9 @@ do
     for READ_COLUMN_STR in $COLUMNS_STR
     do
         echo "=================================================================="
-        echo "Row group size:" $ROW_GROUP_SIZE
         echo "I/O buffer size:" $IO_BUFFER_SIZE
         echo "Read columns str:" $READ_COLUMN_STR
+	echo "Trevni test class:" $TREVNI_TEST_CLASS
         echo "free && sync && echo 3 > /proc/sys/vm/drop_caches && free"|sudo su > /dev/null
         iostat -d -t $DEVICE
         java -jar ../target/tableplacement-experiment-0.0.1-SNAPSHOT.jar $TREVNI_TEST_CLASS -t $TABLE -i $DIR/$TREVNI_PREFIX.$FILE_PREFIX.c$ROW_COUNT -p read.column.string $READ_COLUMN_STR -p io.file.buffer.size $IO_BUFFER_SIZE
