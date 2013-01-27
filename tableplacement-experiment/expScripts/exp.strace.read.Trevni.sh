14c4fc4ede4818d7d64f2a69e2f1e2c5a9a7005b
yhuai <huaiyin.thu@gmail.com>
2013-01-27 11:54:44 -0500

    R plot

--- tableplacement-experiment/expScripts/exp.strace.read.Trevni.sh
+++ tableplacement-experiment/expScripts/exp.strace.read.Trevni.sh
@@ -28,8 +28,8 @@ echo "=================================================================="
 echo "I/O buffer size:" $IO_BUFFER_SIZE
 echo "Read columns str:" $READ_COLUMN_STR
 echo "Trevni test class:" $TREVNI_TEST_CLASS
-echo "free && sync && echo 3 > /proc/sys/vm/drop_caches && free"|sudo su > /dev/null
+echo "free && sync && echo 3 > /proc/sys/vm/drop_caches && free"|sudo su #> /dev/null
 iostat -d -t $DEVICE
 strace -F -f -ttt -T -o $OUT_DIR/strace.$TREVNI_PREFIX.$FILE_PREFIX.c$ROW_COUNT.io$IO_BUFFER_SIZE.out java -jar ../target/tableplacement-experiment-0.0.1-SNAPSHOT.jar $TREVNI_TEST_CLASS -t $TABLE -i $DIR/$TREVNI_PREFIX.$FILE_PREFIX.c$ROW_COUNT -p read.column.string $READ_COLUMN_STR -p io.file.buffer.size $IO_BUFFER_SIZE
-echo "free && sync && echo 3 > /proc/sys/vm/drop_caches && free"|sudo su > /dev/null
+echo "free && sync && echo 3 > /proc/sys/vm/drop_caches && free"|sudo su #> /dev/null
 iostat -d -t $DEVICE
