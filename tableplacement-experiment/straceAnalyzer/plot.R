0ea91b26be89b2b714f5736b976facd2c502b893
yhuai <huaiyin.thu@gmail.com>
2013-01-27 12:06:40 -0500

    wip

--- tableplacement-experiment/straceAnalyzer/plot.R
+++ tableplacement-experiment/straceAnalyzer/plot.R
@@ -8,4 +8,4 @@ d <- read.table(file, header=TRUE)
 
 #d=data.frame(x=c(1,2,5,6,8), y=c(3,6,2,8,7), vx=c(1,1.5,0.8,0.5,1.3), vy=c(0.2,1.3,1.7,0.8,1.4))
 ggplot() + 
-geom_segment(data=d, mapping=aes(x=startTS*1000, y=startPos/1024, xend=endTS*1000, yend=endPos/1024), size=1, color="black", alpha = 1) + scale_y_continuous(trans = log2_trans()) + ylab("File Position (KiB)") + xlab("Time (ms)") + ggtitle("Time Series")
+geom_segment(data=d, mapping=aes(x=startTS*1000, y=startPos/1024, xend=endTS*1000, yend=endPos/1024), size=1, color="black", alpha = 1) + ylab("File Position (KiB)") + xlab("Time (ms)") + ggtitle("Time Series")
