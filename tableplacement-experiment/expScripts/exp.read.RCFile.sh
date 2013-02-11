b25ea1cd61086bd90271ff881779fda80c76b13f
yhuai <huaiyin.thu@gmail.com>
2013-02-11 16:11:50 -0500

    update

---
+++ tableplacement-experiment/expScripts/exp.read.RCFile.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+if [ $# -ne 4 ]
+then
+  echo "./exp.strace.read.RCFile.sh <exp> <row group size> <io buffer size> <read column str>"
+  echo "<exp>: exp1, exp2, exp3, ..."
+  exit
+fi
+
+EXP=$1
+ROW_GROUP_SIZE=$2
+IO_BUFFER_SIZE=$3
+READ_COLUMN_STR=$4
+
+EXP_COMMON_CONF_PATH="./expConf/common.conf"
+echo "Loading parameters from $EXP_COMMON_CONF_PATH"
+source $EXP_COMMON_CONF_PATH
+
+EXP_CONF_PATH="./expConf/$EXP.conf"
+echo "Loading parameters from $EXP_CONF_PATH"
+source $EXP_CONF_PATH
+
+echo "Printing system infomation ..."
+uname -a
+cat /etc/lsb-release
+
+echo "=================================================================="
+echo "Row group size:" $ROW_GROUP_SIZE
+echo "I/O buffer size:" $IO_BUFFER_SIZE
+echo "Read columns str:" $READ_COLUMN_STR
+echo "free && sync && echo 3 > /proc/sys/vm/drop_caches && free"|sudo su #> /dev/null
+iostat -d -t $DEVICE
+java -jar ../target/tableplacement-experiment-0.0.1-SNAPSHOT.jar ReadRCFile -t $TABLE -i $DIR/$RCFILE_PREFIX.$FILE_PREFIX.c$ROW_COUNT.rg$ROW_GROUP_SIZE -p read.column.string $READ_COLUMN_STR -p io.file.buffer.size $IO_BUFFER_SIZE $OTHER_PROPERTIES
+echo "free && sync && echo 3 > /proc/sys/vm/drop_caches && free"|sudo su #> /dev/null
+iostat -d -t $DEVICE
---
+++ tableplacement-experiment/expScripts/exp.read.Trevni.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+if [ $# -ne 5 ]
+then
+  echo "./exp.strace.read.Trevni.sh <exp> <io buffer size> <read column str> <access pattern>"
+  echo "<exp>: exp1, exp2, exp3, ..."
+  echo "<access pattern>: r is row-oriented access; c is column-oriented access"
+  exit
+fi
+
+EXP=$1
+IO_BUFFER_SIZE=$2
+READ_COLUMN_STR=$3
+ACCESS=$4
+
+EXP_COMMON_CONF_PATH="./expConf/common.conf"
+echo "Loading parameters from $EXP_COMMON_CONF_PATH"
+source $EXP_COMMON_CONF_PATH
+
+EXP_CONF_PATH="./expConf/$EXP.conf"
+echo "Loading parameters from $EXP_CONF_PATH"
+source $EXP_CONF_PATH
+
+# override TREVNI_TEST_CLASS based on input parameter
+TREVNI_TEST_CLASS="ReadTrevniColumnOriented"
+if [ $ACCESS == "r" ]
+then  	  	
+  TREVNI_TEST_CLASS="ReadTrevniRowOriented"
+fi
+
+echo "Printing system infomation ..."
+uname -a
+cat /etc/lsb-release
+
+echo "=================================================================="
+echo "I/O buffer size:" $IO_BUFFER_SIZE
+echo "Read columns str:" $READ_COLUMN_STR
+echo "Trevni test class:" $TREVNI_TEST_CLASS
+echo "free && sync && echo 3 > /proc/sys/vm/drop_caches && free"|sudo su #> /dev/null
+iostat -d -t $DEVICE
+java -jar ../target/tableplacement-experiment-0.0.1-SNAPSHOT.jar $TREVNI_TEST_CLASS -t $TABLE -i $DIR/$TREVNI_PREFIX.$FILE_PREFIX.c$ROW_COUNT -p read.column.string $READ_COLUMN_STR -p io.file.buffer.size $IO_BUFFER_SIZE $OTHER_PROPERTIES
+echo "free && sync && echo 3 > /proc/sys/vm/drop_caches && free"|sudo su #> /dev/null
+iostat -d -t $DEVICE
