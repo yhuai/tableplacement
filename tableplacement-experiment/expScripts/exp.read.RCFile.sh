52bbb3659211cfb450be25a1f2ab997e5065b13a
yhuai <huaiyin.thu@gmail.com>
2013-02-12 11:23:39 -0500

    update

--- tableplacement-experiment/expScripts/exp.read.RCFile.sh
+++ tableplacement-experiment/expScripts/exp.read.RCFile.sh
@@ -29,7 +29,7 @@ echo "Row group size:" $ROW_GROUP_SIZE
 echo "I/O buffer size:" $IO_BUFFER_SIZE
 echo "Read columns str:" $READ_COLUMN_STR
 echo "free && sync && echo 3 > /proc/sys/vm/drop_caches && free"|sudo su #> /dev/null
-iostat -d -t -p
+iostat -d -t $DEVICE
 java -jar ../target/tableplacement-experiment-0.0.1-SNAPSHOT.jar ReadRCFile -t $TABLE -i $DIR/$RCFILE_PREFIX.$FILE_PREFIX.c$ROW_COUNT.rg$ROW_GROUP_SIZE -p read.column.string $READ_COLUMN_STR -p io.file.buffer.size $IO_BUFFER_SIZE $OTHER_PROPERTIES
 echo "free && sync && echo 3 > /proc/sys/vm/drop_caches && free"|sudo su #> /dev/null
-iostat -d -t -p
+iostat -d -t $DEVICE
